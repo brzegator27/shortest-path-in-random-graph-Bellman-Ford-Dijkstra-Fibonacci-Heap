a1616a22b8dc56bb13eb628f1ac85005be6e8665
Jakub Brzegowski <symfiz@gmail.com>
2015-10-24 10:30:11 +0200

    Added reading from file

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -5,10 +5,7 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,18 +13,100 @@ import java.util.stream.Stream;
  * Created by Jakub on 2015-10-22.
  */
 public abstract class Graph {
-//    private LinkedList<Vertex> vertices;
     String filenameInput, filenameOutput;
+    String prefixInEdgeNotation = "[";
+    String postfixInEdgeNotation = "]";
+    String splitterInEdgeNotation = ",";
 
     public abstract void generateRandomGraph(int verticesCount, int edgesCount);
     public abstract void addVertex(Vertex vertex);
     public abstract void addEdge(Edge edge);
-    public abstract void addEdgeToVertex(Vertex startVertex, Vertex endVertex, Integer connectingEdgeWeight);
-    public abstract void addEdgeToVertex(Integer startVertexLabel, Integer endVertexLabel, Integer connectingEdgeWeight);
+//    public abstract void addEdgeToVertex(Vertex startVertex, Vertex endVertex, Integer connectingEdgeWeight);
+//    public abstract void addEdgeToVertex(Integer startVertexLabel, Integer endVertexLabel, Integer connectingEdgeWeight);
     public abstract Vertex getVertexByLabel(Integer label);
     public abstract void inicializeSingleSource(Integer startVertexLabel, Integer startVertexDistance);
     public abstract Iterable<Vertex> getIterableList();
     public abstract Integer getVertexCount();
+
+
+    public void setFilenames(String filenameInput, String filenameOutput) {
+        this.filenameInput = filenameInput;
+        this.filenameOutput = filenameOutput;
+    }
+
+    public void readGraph() {
+        Path path = Paths.get(this.filenameInput);
+        String edgeAsString;
+        String edgeAsStringTrimmed;
+        String[] edgeProperties;
+        int edgeAsStringLength;
+        int prefixLength = this.prefixInEdgeNotation.length();
+        int postfixLength = this.postfixInEdgeNotation.length();
+        HashMap<Integer, Vertex> verticesFromFile = new HashMap<>();
+
+        try {
+            Stream<String> lines = Files.lines(path);
+            List<String> linesInList;
+            ListIterator<String> iterator;
+
+            linesInList = lines.collect(Collectors.toList());
+            iterator = linesInList.listIterator();
+
+            while(iterator.hasNext()) {
+                edgeAsString = iterator.next();
+                edgeAsStringLength = edgeAsString.length();
+                edgeAsStringTrimmed = edgeAsString.substring(prefixLength, edgeAsStringLength - postfixLength);
+                edgeProperties = edgeAsStringTrimmed.split(this.splitterInEdgeNotation, 3);
+
+                this.manageStrignToEdge(edgeProperties, verticesFromFile);
+            }
+        } catch (IOException e) {
+            System.out.print("Exception in Graph::readGraph");
+        }
+    }
+
+    protected void manageStrignToEdge(String[] edgeProperties, HashMap<Integer, Vertex> vertices) {
+        Integer startVertexLabel = Integer.parseInt(edgeProperties[0]),
+                endVertexLabel = Integer.parseInt(edgeProperties[1]),
+                edgeWeight = Integer.parseInt(edgeProperties[2]);
+        Vertex startVertex = this.manageVertexInHashMap(startVertexLabel, vertices),
+                endVertex = this.manageVertexInHashMap(endVertexLabel, vertices);
+        Edge newEdge = new Edge(startVertex, endVertex, edgeWeight);
+
+        startVertex.addEdge(newEdge);
+    }
+
+    protected Vertex manageVertexInHashMap(Integer vertexLabel, HashMap<Integer, Vertex> vertices) {
+        Vertex newVertex;
+
+        if(vertices.containsKey(vertexLabel)) {
+            return vertices.get(vertexLabel);
+        } else {
+            newVertex = new Vertex(vertexLabel);
+            vertices.put(vertexLabel, newVertex);
+            return newVertex;
+        }
+    }
+
+    public void saveGraph() {
+        try{
+            PrintWriter writer = new PrintWriter(this.filenameOutput, "UTF-8");
+
+//            ListIterator<Entry> iterator = dict.listIterator();
+
+//            while(iterator.hasNext()) {
+//                Entry entry = iterator.next();
+//                writer.println(entry.getWord());
+//                writer.println(entry.getClue());
+//            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.print("Exception in Graph::saveGraph");
+        }
+    }
+}
+
 
 //    public Graph() {
 //        this.vertices = new LinkedList<>();
@@ -41,57 +120,3 @@ public abstract class Graph {
 //    public void addVertex(Vertex vertex) {
 //        this.vertices.add(vertex);
 //    }
-
-    public void setFilenames(String filenameInput, String filenameOutput) {
-        this.filenameInput = filenameInput;
-        this.filenameOutput = filenameOutput;
-    }
-
-
-    protected void readGraph(String filename) {
-//        dict = new LinkedList<Entry>();
-        Path path = Paths.get(filename);
-
-        try {
-            Stream<String> lines = Files.lines(path);
-//			boolean isWord = true;
-            String word, clue;
-            List<String> linesInList;
-            ListIterator<String> iterator;
-
-            linesInList = lines.collect(Collectors.toList());
-            iterator = linesInList.listIterator();
-
-            while(iterator.hasNext()) {
-                word = iterator.next();
-                if( ! iterator.hasNext()) {
-                    return;
-                }
-                clue = iterator.next();
-
-//                add(word, clue);
-            }
-
-        } catch (IOException e) {
-
-        }
-    }
-
-    public void saveGraph(String filename) {
-        try{
-            PrintWriter writer = new PrintWriter(filename, "UTF-8");
-
-//            ListIterator<Entry> iterator = dict.listIterator();
-
-//            while(iterator.hasNext()) {
-//                Entry entry = iterator.next();
-//                writer.println(entry.getWord());
-//                writer.println(entry.getClue());
-//            }
-
-            writer.close();
-        } catch (IOException e) {
-
-        }
-    }
-}
