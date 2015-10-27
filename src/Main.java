87bb7118fdfc8e3b89246e1a25a5fdb273070aab
Jakub Brzegowski <symfiz@gmail.com>
2015-10-27 12:37:41 +0100

    Small changes made for presentation for assignment

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,39 @@ import Graph.GraphFibonacciHeap;
 import bellman_ford.BellmanFord;
 import dijkstra.Dijkstra;
 
+import java.util.Random;
+
 /**
  * Created by Jakub on 2015-10-22.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 //        String fileNameIn = "C:\\Users\\Jakub\\Documents\\Java projects\\shortestPathInRandomGraphBFDFH\\resources\\graph.txt";
-        String fileNameIn = "C:\\Users\\Jakub\\Documents\\Java projects\\shortestPathInRandomGraphBFDFH\\resources\\simple_test_in.txt";
+        String fileNameIn = "C:\\Users\\Jakub\\Documents\\Java projects\\shortestPathInRandomGraphBFDFH\\resources\\simple_test_out.txt";
         String fileNameOut = "C:\\Users\\Jakub\\Documents\\Java projects\\shortestPathInRandomGraphBFDFH\\resources\\simple_test_out.txt";
 
         //  "C:\\Users\\Jakub\\Documents\\Java - SBobek\\class 2\\home\\exercise 1\\crossword\\dictionary\\cwdb.txt"
-//        GraphArrayList graph_1 = new GraphArrayList();
-        GraphFibonacciHeap graph_1 = new GraphFibonacciHeap();
+        GraphArrayList graph_1 = new GraphArrayList(),
+                graph_2 = new GraphArrayList();
+//        GraphFibonacciHeap graph_1 = new GraphFibonacciHeap();
 
         graph_1.setFilenames(fileNameIn, fileNameOut);
-//        graph_1.generateRandomGraph(10000, 100);
-        graph_1.readGraph();
-//        graph_1.saveGraph();
+        Random generator = new Random();
+        Integer verticesNumber = 10000,
+                additionalEdgesNumber = 500000,
+                sourceVertexLabel = generator.nextInt(verticesNumber) + 1,
+                endVertexLabel = generator.nextInt(verticesNumber) + 1;
+        graph_1.generateRandomGraph(10000, additionalEdgesNumber);
+        graph_1.saveGraph();
 
+        graph_2.setFilenames(fileNameIn, fileNameOut);
+        graph_2.readGraph();
 
 //        new BellmanFord(graph_1, 1, 4);
-        new Dijkstra(graph_1, 1, 4);
-        graph_1.showBestRoute(1, 4);
+        new Dijkstra(graph_2, sourceVertexLabel, endVertexLabel);
+        graph_2.showBestRoute(sourceVertexLabel, endVertexLabel);
 //        new BellmanFord(graph_1, 1708, 774);
 //        graph_1.showBestRoute(1708, 774);
 //        System.out.print(graph_1.toString());
-
-
-//        graph_1.toString();
-//        System.out.println(graph_1.getVertexByLabel(1).getParentVertex().getLabel());
-//        System.out.println("parent of 2 is: " + graph_1.getVertexByLabel(2).getParentVertex().getLabel());
-//        System.out.println("parent of 3 is: " + graph_1.getVertexByLabel(3).getParentVertex().getLabel());
-//        System.out.println("parent of 4 is: " + graph_1.getVertexByLabel(4).getParentVertex().getLabel());
-//        System.out.println("parent of 5 is: " + graph_1.getVertexByLabel(5).getParentVertex().getLabel());
     }
 }
