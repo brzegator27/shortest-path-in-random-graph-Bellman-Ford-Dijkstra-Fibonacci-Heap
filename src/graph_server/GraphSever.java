30212d688033adfc7eda0dc8e74ba567b087e0c9
Jakub Brzegowski <symfiz@gmail.com>
2015-10-29 22:49:47 +0100

    Added file structure to implement server which will be sending generated graph to another program

---
+++ src/graph_server/GraphSever.java
@@ -0,0 +1,10 @@
+package graph_server;
+
+/**
+ * Created by Jakub on 2015-10-29.
+ */
+public class GraphSever {
+    public static void main(String[] args) {
+        System.out.println("Hello World main in GraphServer");
+    }
+}
